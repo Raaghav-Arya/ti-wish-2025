d8e55de7a6b754ffe8f7ae754dae4fe20bffe728
RiyaK0912 <riyakumar2709@gmail.com>
2025-06-03 14:12:14 +0530

    add calculator project
    Signed-off-by: RiyaK0912 <riyakumar2709@gmail.com>

---
+++ calculator_project/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "../inc/add.h"
+#include "../inc/sub.h"
+#include "../inc/mul.h"
+#include "../inc/div.h"
+#include "../inc/menu.h"
+
+int main()
+{
+    printf("\n\rWelcome to the Calculator Project\n\r");
+    return 0;
+}
